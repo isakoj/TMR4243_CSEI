6223b40ab731153ca07fcd77eca0ab5565fedd68
Isak Øvsthus Jordal <156349018+isakoj@users.noreply.github.com>
2024-04-16 09:25:19 +0200

    Update luenberg.py
    Added feedback for the estimated states

--- template_observer/template_observer/luenberg.py
+++ template_observer/template_observer/luenberg.py
@@ -3,16 +3,17 @@
 import numpy as np
 from template_observer.wrap import wrap
 
-def luenberg(eta, tau, L1, L2, L3):
-
-    eta_hat =   np.array([0, 0, 0])
-    nu_hat  =   np.array([0, 0, 0])
-    bias_hat   =   np.array([0, 0, 0])
+def luenberg(eta, tau, observer, L1, L2, L3):
+    
+    eta_hat = observer.eta
+    nu_hat = observer.nu
+    bias_hat = observer.bias
 
     # Log the incoming information
     print("=== Luenberg Observer ===")
     print("eta: ", eta)
     print("tau: ", tau)
+    print("observer", observer)
     print("L1: ", L1)
     print("L2: ", L2)
     print("L3: ", L3)
